4a93a468e665638c1c3ba7251c337feb2437958c
heziresheff <yehezkel.resheff@mail.huji.ac.il>
2015-11-25 22:04:46 +0200

    Full LI data

--- ptncode/linkedin.py
+++ ptncode/linkedin.py
@@ -1,43 +1,82 @@
 
-import os
-import json
-import requests
+import urllib2
 from bs4 import BeautifulSoup
+import json
 
-PEOPLE_JSON = "\\data\\people-linkedin.json"
-HTML_OUT = "\\data\\html"
 
-client = requests.Session()
+PEOPLE_JSON = "c:\\datahack\\data\\people-linkedin.json"
+people_json = json.load(open(PEOPLE_JSON, 'r'))
 
-HOMEPAGE_URL = 'https://www.linkedin.com'
-LOGIN_URL = 'https://www.linkedin.com/uas/login-submit'
+# all people go here
+people = dict()
 
-html = client.get(HOMEPAGE_URL).content
-soup = BeautifulSoup(html)
-csrf = soup.find(id="loginCsrfParam-login")['value']
-
-login_information = {
-    'session_key': ?,
-    'session_password': ?,
-    'loginCsrfParam': csrf,
-}
-
-client.post(LOGIN_URL, data=login_information)
-
-people = json.load(open(PEOPLE_JSON, 'r'))
-for person in people.keys():
-    linkedin_url = people[person]
-    print person, linkedin_url
-    if not "https" in linkedin_url:
+for person in people_json.keys():
+    print person
+    
+    url_to_get = people_json[person]
+    if not "www" in url_to_get:
         continue
-    else:
-        linkedin_url = linkedin_url.replace("il.link", "www.link")
-        linkedin_url = linkedin_url.replace("th.link", "www.link")
-        linkedin_url = linkedin_url.replace("ru.link", "www.link")
-        linkedin_url = linkedin_url.replace("uk.link", "www.link")
 
-        page = client.get(linkedin_url)
-        html = page.content
-        output_file = linkedin_url.split("/")[-1]
-        with open(os.path.join(HTML_OUT, "{}.html".format(output_file)), "w") as out:
-            out.write(html)
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36')]
+
+    page = opener.open(url_to_get).read()
+    soup = BeautifulSoup(page, "lxml")
+
+    # Find all skills
+    skills = []
+    skill_elements = soup.findAll('li', attrs={'class':"skill"})
+    for el in skill_elements:
+        skills.append(el.getText())
+
+    #GET SUMMARY
+    try:
+        summary_soup = soup.findAll('section', attrs={'id': "summary"})
+        summary = summary_soup[0].findAll('div', attrs={'class': "description"})[0].get_text()
+    except:
+        summary = ""
+
+    # GET JOBS
+    job_positions_list = soup.findAll('li',attrs={'class':  "position"})
+    job_dicts_list = []
+    for job in job_positions_list:
+        job_dict = {}
+        job_title = job.findAll(attrs={'class': "item-title"})
+        job_place =  job.findAll(attrs={'class': "item-subtitle"})
+        job_descrip =  job.findAll(attrs={'class': "description"})
+        job_dict["job_title"] = job_title[0].get_text()
+        job_dict["job_place"] = job_place[0].get_text()
+        if len(job_descrip)>0:
+            job_dict["job_descrip"] = job_descrip[0].get_text()
+        else:
+            job_dict["job_descrip"] = None
+
+        job_dicts_list.append(job_dict)
+
+    #GET EDS
+    edu_list = soup.findAll('li',attrs={'class':"school"})
+    edu_dicts_list = []
+    for ed in edu_list:
+        ed_dict = {}
+        ed_title = ed.findAll(attrs={'class':"item-title"})
+        ed_place =  ed.findAll(attrs={'class':"item-subtitle"})
+        ed_descrip =  ed.findAll(attrs={'class':"description"})
+        ed_dict["ed_place"] = ed_title[0].get_text()
+        ed_dict["ed_title"] = ed_place[0].get_text()
+        if len(ed_descrip)>0:
+            ed_dict["ed_descrip"] = ed_descrip[0].get_text()
+        else:
+            ed_dict["ed_descrip"] = None
+
+        edu_dicts_list.append(ed_dict)
+
+
+    this_person = {"LinkedIn": url_to_get,
+                   "skills": skills,
+                   "jobs": job_dicts_list,
+                   "eds": edu_dicts_list,
+                   "summary": summary}
+
+    people[person] = this_person
+
+json.dump(people, open("with_skills.json", "w"), indent=4)
